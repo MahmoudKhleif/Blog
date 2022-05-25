be860aa89eab1e74f2fbc0434dd33cce185947be
MahmoudKhleif <Mahmoud89ah@gmail.com>
2022-05-26 01:59:48 +0200

    Add DataBase

--- Blog/Data/ApplicationDbContext.cs
+++ Blog/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Blog.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Blog.Data
@@ -9,5 +10,6 @@ namespace Blog.Data
             : base(options)
         {
         }
+        public DbSet<Card> Cards { get; set; }
     }
 }
---
+++ Blog/Models/Card.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Blog.Models
+{
+    public class Card
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        [Required,MinLength(250),MaxLength(2500)]
+        public string Story { get; set; }
+        public DateTime DateTime { get; set; }
+        public string? Image { get; set; }
+    }
+}
